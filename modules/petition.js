9d79c5e146e31986f5fdcbcdd462629d70075f83
Sam Keddy <skeddy13@yahoo.com>
2023-01-02 12:31:46 -0500

    clean slate

--- modules/petition.js
+++
@@ -1,7 +0,0 @@
-
-//before
-new Module('petition', 'message', /^!?petition.*/i, function (message) {
-	react(message, ['⬆️','⬇️' ]);
-});
-
-/*global Module, log, error, send, react, sendEmoji, pickRandom, messageHasBotPing, isMod */
